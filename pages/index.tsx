f874bed265fba3a196320dbe090d94160618658c
tommyrharper <thomasrobertharper@gmail.com>
2022-05-14 16:59:46 +0100

    feat: render collection names

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,10 @@ import { gql } from "@apollo/client";
 import type { GetServerSidePropsContext, NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { GetNftCollectionsDocument, useGetNftCollectionsQuery } from "../generated/graphql";
+import {
+  GetNftCollectionsDocument,
+  useGetNftCollectionsQuery,
+} from "../generated/graphql";
 import { addApolloState, initializeApollo } from "../lib/apolloClient";
 import styles from "../styles/Home.module.css";
 
@@ -10,7 +13,35 @@ const Home: NextPage = () => {
   const { loading, data } = useGetNftCollectionsQuery();
 
   return (
-    <div className="bg-gray-50">
+    <div className="bg-neutral-50 h-screen">
+      <div className="flex mb-4 justify-center items-center h-3/4">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-cotton-50">
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">Choose a collection</div>
+            {data?.getNFTCollections.map((collection) => {
+              return (
+                <p className="text-gray-700 text-base" key={collection.uuid}>
+                  {collection.name}
+                </p>
+              );
+            })}
+
+          </div>
+          <div className="px-6 pt-4 pb-2">
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              #photography
+            </span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              #travel
+            </span>
+            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+              #winter
+            </span>
+          </div>
+        </div>
+      </div>
+
+      {/* <div>test</div>
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
         <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
           <span className="block">Ready to dive in?</span>
@@ -36,7 +67,7 @@ const Home: NextPage = () => {
             </a>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 module.exports = {
   content: [
-    // Example content paths...
-    // './public/**/*.html',
-    // './**/*.{js,jsx,ts,tsx}',
-    // "./pages/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    // 'apps/site/pages/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cotton: {
+          50: '#F5F4F2',
+        }
+      },
+    },
   },
   variants: {},
   plugins: [],
