f2ea3b5a47cb401530dda2f110c6f3e83ea7cda3
tommyrharper <thomasrobertharper@gmail.com>
2022-05-15 16:53:11 +0100

    feat: add frame text to reveal screen

--- pages/reveal/index.tsx
+++ pages/reveal/index.tsx
@@ -1,6 +1,9 @@
 import { ReactElement } from "react";
 import { NextPageWithLayout } from "../../utils/types";
 import RevealImage from "../../components/reveal-image";
+import BackButton from "../../components/back-button";
+import Image from "next/Image";
+import FRAME from "../../public/FRAME.png";
 
 const Reveal: NextPageWithLayout<{}> = () => {
   return (
@@ -9,19 +12,18 @@ const Reveal: NextPageWithLayout<{}> = () => {
       <RevealImage />
       <RevealImage />
       <RevealImage />
-      {/* <BackButton href="/" /> */}
     </div>
   );
 };
 
 Reveal.getLayout = function getLayout(page: ReactElement) {
   return (
-    <div className="bg-neutral-50 h-screen">
-      {/* <div className="flex flex-row mb-4 justify-center items-center h-3/4"> */}
-      {/* <div className="overflow-hidden w-1/3"> */}
-      {/* </div> */}
-      {/* </div> */}
-      <div className="h-full flex flex-col justify-center px-40">{page}</div>
+    <div className="bg-neutral-50 grid grid-cols-1 h-screen">
+      <div className="w-32 mt-7 ml-7 -mb-7">
+        <Image src={FRAME} alt="loading" layout="intrinsic" />
+      </div>
+      <div className="flex flex-col justify-center px-28">{page}</div>
+      <div></div>
     </div>
   );
 };
