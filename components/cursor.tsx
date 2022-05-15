8ea196255197e527bb4627be4db6d35cfe943349
tommyrharper <thomasrobertharper@gmail.com>
2022-05-15 16:00:54 +0100

    fix: hover text flickering pointer event bug

--- components/cursor.tsx
+++ components/cursor.tsx
@@ -6,18 +6,22 @@ const Cursor = () => {
 
   useEffect(() => {
     const handleMouseMove = (event: MouseEvent) => {
-      setXCoord(event.pageX);
-      setYCoord(event.pageY);
+      const { pageX, pageY } = event;
+      if (pageX !== 0) setXCoord(event.pageX);
+      if (pageY !== 0) setYCoord(event.pageY);
     };
     document.addEventListener("mousemove", handleMouseMove);
 
     return () => document.removeEventListener("mousemove", handleMouseMove);
   });
 
+  // console.log(xCoord, yCoord);
+
   return (
     <div
-      className={`absolute -translate-x-2/4 -translate-y-full`}
-      style={{ top: yCoord, left: xCoord }}
+      className="absolute -translate-x-2/4 -translate-y-full z-10"
+      // className="absolute z-10"
+      style={{ top: yCoord, left: xCoord,  pointerEvents: "none"  }}
     >
       <div>CLICK TO REVEAL</div>
     </div>
--- components/reveal-image.tsx
+++ components/reveal-image.tsx
@@ -4,29 +4,30 @@ import nft from "../public/nft.png";
 import Cursor from "./cursor";
 
 const RevealImage = () => {
+  // TODO: refactor visible to be blur
   const [visible, setVisible] = useState<boolean>(false);
   const [showCursor, setShowCursor] = useState<boolean>(false);
 
   return (
     <div className="mx-9">
-      {showCursor && <Cursor />}
       <div
         className="cursor-pointer"
         onMouseEnter={() => {
-          setShowCursor(true);
+          if (!showCursor) {
+            setShowCursor(true);
+          }
         }}
-        onMouseLeave={() => {
-          setShowCursor(false);
+        onMouseOut={() => {
+          if (showCursor) {
+            setShowCursor(false);
+          }
         }}
       >
+      {showCursor && <Cursor />}
         <Image
           src={nft}
           alt="loading"
           className={`transition-all duration-1500 ${visible ? "" : "blur-lg"}`}
-          onClick={() => {
-            setShowCursor(false);
-            setVisible(true);
-          }}
         />
       </div>
     </div>
