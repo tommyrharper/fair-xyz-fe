4dd9c502c17c83343a87ab25b0a706c0160ee95b
tommyrharper <thomasrobertharper@gmail.com>
2022-05-15 14:08:53 +0100

    feat: add images to reveal page

--- pages/reveal/index.tsx
+++ pages/reveal/index.tsx
@@ -1,23 +1,41 @@
 import { ReactElement } from "react";
+import Image from "next/Image";
 import BackButton from "../../components/back-button";
 import Header from "../../components/header";
 import { DefaultLayout } from "../../layouts/default";
 import { NextPageWithLayout } from "../../utils/types";
+import nft from "../../public/nft.png";
+
+const NFTImage = () => {
+  return (
+    <div className="mx-9">
+      <Image src={nft} alt="loading" />
+    </div>
+  );
+};
 
 const Reveal: NextPageWithLayout<{}> = () => {
-
   return (
-    <>
-      <Header text="Reveal releases" />
-
-
-      <BackButton href="/" />
-    </>
+    <div className="flex">
+      <NFTImage />
+      <NFTImage />
+      <NFTImage />
+      <NFTImage />
+      {/* <BackButton href="/" /> */}
+    </div>
   );
 };
 
 Reveal.getLayout = function getLayout(page: ReactElement) {
-  return <DefaultLayout>{page}</DefaultLayout>;
+  return (
+    <div className="bg-neutral-50 h-screen">
+      {/* <div className="flex flex-row mb-4 justify-center items-center h-3/4"> */}
+      {/* <div className="overflow-hidden w-1/3"> */}
+      {/* </div> */}
+      {/* </div> */}
+      <div className="h-full flex flex-col justify-center px-40">{page}</div>
+    </div>
+  );
 };
 
 export default Reveal;
