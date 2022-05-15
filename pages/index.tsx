351e3377e75656d654a6daf4155540fce5aeda33
tommyrharper <thomasrobertharper@gmail.com>
2022-05-15 20:04:25 +0100

    style: add clean 50/50 divide on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,5 @@
 import { ReactElement } from "react";
-// import Button from "../components/button";
-import { DefaultLayout } from "../layouts/default";
-import { RevealLayout } from "../layouts/reveal-layout";
 import { NextPageWithLayout } from "../utils/types";
-import Image from "next/Image";
-import FRAME from "../public/FRAME.png";
 import Link from "next/link";
 
 interface ButtonProps {
@@ -18,7 +13,7 @@ const Button = ({ text, href, disabled = false }: ButtonProps) => {
     <Link href={href} className="flex-col items-center justify-center h-full">
       <button
         type="button"
-        className="border rounded-lg border-carbon hover:text-cotton hover:bg-carbon text-carbon w-full h-4/5"
+        className="hover:text-cotton hover:bg-carbon text-carbon w-full h-full"
         disabled={disabled}
       >
         <p className="font-NeueMontreal text-2xl">{text.toUpperCase()}</p>
@@ -30,12 +25,12 @@ const Button = ({ text, href, disabled = false }: ButtonProps) => {
 const Home: NextPageWithLayout<{}> = () => {
   return (
     <div className="bg-neutral-50 h-screen">
-      <div className="grid grid-cols-1 h-full">
-        <div className="flex flex-col justify-center px-28 pt-10">
-          <Button text="Upcoming releases" href={`/upcoming`} />
+      <div className="grid grid-cols-1 h-full divide-y-2">
+        <div className="flex flex-col justify-center">
+          <Button text="UPCOMING RELEASES" href={`/upcoming`} />
         </div>
-        <div className="flex flex-col justify-center px-28 pb-10">
-          <Button text="Reveal new nfts!" href={`/reveal`} />
+        <div className="flex flex-col justify-center">
+          <Button text="REVEAL NEW NFTS" href={`/reveal`} />
         </div>
       </div>
     </div>
