e9646efe7decedaade85083deff1205aed071e78
tommyrharper <thomasrobertharper@gmail.com>
2022-05-15 20:00:56 +0100

    style: update home page styling to use big buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,45 @@
 import { ReactElement } from "react";
-import Button from "../components/button";
+// import Button from "../components/button";
 import { DefaultLayout } from "../layouts/default";
+import { RevealLayout } from "../layouts/reveal-layout";
 import { NextPageWithLayout } from "../utils/types";
+import Image from "next/Image";
+import FRAME from "../public/FRAME.png";
+import Link from "next/link";
 
-const Home: NextPageWithLayout<{}> = () => {
+interface ButtonProps {
+  text: string;
+  href: string;
+  disabled?: boolean;
+}
+
+const Button = ({ text, href, disabled = false }: ButtonProps) => {
   return (
-    <>
-      <Button text="Upcoming releases" href={`/upcoming`} />
-      <Button text="Reveal new nfts!" href={`/reveal`} />
-    </>
+    <Link href={href} className="flex-col items-center justify-center h-full">
+      <button
+        type="button"
+        className="border rounded-lg border-carbon hover:text-cotton hover:bg-carbon text-carbon w-full h-4/5"
+        disabled={disabled}
+      >
+        <p className="font-NeueMontreal text-2xl">{text.toUpperCase()}</p>
+      </button>
+    </Link>
   );
 };
 
-Home.getLayout = function getLayout(page: ReactElement) {
-  return <DefaultLayout>{page}</DefaultLayout>;
+const Home: NextPageWithLayout<{}> = () => {
+  return (
+    <div className="bg-neutral-50 h-screen">
+      <div className="grid grid-cols-1 h-full">
+        <div className="flex flex-col justify-center px-28 pt-10">
+          <Button text="Upcoming releases" href={`/upcoming`} />
+        </div>
+        <div className="flex flex-col justify-center px-28 pb-10">
+          <Button text="Reveal new nfts!" href={`/reveal`} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
