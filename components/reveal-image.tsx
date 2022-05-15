ba1ba5512220858e170510e4e363d113854b617d
tommyrharper <thomasrobertharper@gmail.com>
2022-05-15 16:15:20 +0100

    refactor: update visible variable to blur

--- components/reveal-image.tsx
+++ components/reveal-image.tsx
@@ -4,14 +4,14 @@ import nft from "../public/nft.png";
 import Cursor from "./cursor";
 
 const RevealImage = () => {
-  // TODO: refactor visible to be blur
-  const [visible, setVisible] = useState<boolean>(false);
+  // TODO: refactor blur to be blur
+  const [blur, setBlur] = useState<boolean>(true);
   const [showCursor, setShowCursor] = useState<boolean>(false);
 
   return (
     <div className="mx-9">
       <div
-        className={!visible ? "cursor-pointer" : undefined}
+        className={blur ? "cursor-pointer" : undefined}
         onMouseMove={() => {
           setShowCursor(true);
         }}
@@ -19,13 +19,13 @@ const RevealImage = () => {
           setShowCursor(false);
         }}
       >
-        {showCursor && !visible && <Cursor />}
+        {showCursor && blur && <Cursor />}
         <Image
           src={nft}
           alt="loading"
-          className={`transition-all duration-1500 ${visible ? "" : "blur-lg"}`}
+          className={`transition-all duration-1500 ${blur ? "blur-lg" : ""}`}
           onClick={() => {
-            setVisible(true);
+            setBlur(false);
           }}
         />
       </div>
